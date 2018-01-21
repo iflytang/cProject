3cdf4cb9665622383c2157c650014b491065f684
iflytang <iflytang@qq.com>
2018-01-21 16:52:55 +0800

    insert node in signle linked list

--- signalLinkedList.c
+++ signalLinkedList.c
@@ -26,16 +26,52 @@ int main() {
     }
     temp->next = NULL;
 
-    /* display the linked list */
+    /* display the original linked list */
+    printf("======================================================\n");
+    temp = root->next;
+    for (int i = 1; i < 15; i++) {
+        printf("node[%d] = %d\t", i, temp->value);
+        temp = temp->next;
+        if (i % 5 == 0) {
+            printf("\n");
+        }
+        if (!temp) {
+            printf("NULL node.\n");
+            break;
+        }
+    }
+
+    /* inser a node */
+    int insert_pos = 5;
+    int insert_value = 0xff;
+    temp = root->next;
+    printf("======================================================\n");
+    for (int i = 1; i < 11; i++) {
+        if (i == insert_pos) {
+            struct node * insert = (struct node *)malloc(sizeof(struct node *));
+            insert->value = insert_value;
+            insert->next = temp->next;
+            temp->next = insert;
+            printf("insert a node ok! insert_pos: %d, insert_value: %d\n", insert_pos, insert->value);
+            break;
+        }
+        temp = temp->next;
+    }
+
+    /* display the inserted linked list */
     printf("======================================================\n");
     temp = root->next;
     struct node *destory;   // release memory to avoid memory leak
-    for (int i = 1; i < 11; i++) {
+    for (int i = 1; i < 15; i++) {
         printf("node[%d] = %d\t", i, temp->value);
         destory = temp;
         temp = temp->next;
         if (i % 5 == 0) {
             printf("\n");
+        }
+        if (!temp) {
+            printf("NULL node.\n");
+            break;
         }
         free(destory);
     }
